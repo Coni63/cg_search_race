946766fabe777f36ee1ac88051d1eedd5568a434
nicolas <mine.nicolas@gmail.com>
2023-03-04 11:27:30 +0100

    fix adapter to ease use with RL

--- ai/adapter.py
+++ ai/adapter.py
@@ -1,24 +1,26 @@
 
+import numpy as np
+
 from game import GameManager, Action
 
 
 class Adapter:
 
     @staticmethod
-    def game_to_state(game: GameManager) -> list[float]:
+    def game_to_state(game: GameManager) -> np.array:
         chkpt_idx = game.pod.nextCheckPointId
         curr_target, next_target = game.checkpoints[chkpt_idx], game.checkpoints[chkpt_idx+1]
         angle1 = game.pod.diffAngle(curr_target)
         angle2 = game.pod.diffAngle(next_target)
         dist1 = game.pod.distance(curr_target)
         dist2 = game.pod.distance(next_target)
-        return [
+        return np.array([
             game.pod.speed / 1133,
             angle1 / 180,
             dist1 / 10000,
             angle2 / 180,
             dist2 / 10000,
-        ]
+        ])
 
     @staticmethod
     def prediction_to_action(output: list[float]) -> Action:
