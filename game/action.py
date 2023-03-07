d73be9de22cde554ae21e2d9dc8a6dd1ad66f282
nicolas <mine.nicolas@gmail.com>
2023-03-07 21:00:44 +0100

    prepare for external output

--- game/action.py
+++ game/action.py
@@ -5,3 +5,6 @@ from dataclasses import dataclass
 class Action:
     thrust: int
     angle: float  # will be converted to X, Y after
+
+    def __str__(self):
+        return f"{self.thrust},{self.angle}"
